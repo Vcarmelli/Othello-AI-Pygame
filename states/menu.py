68d076563a2b3982eb3bf7dbb3c28dba1d3ea4ac
Paul Adrian <pauladrian0224@gmail.com>
2024-05-14 20:14:06 +0800

    Menu fixed

--- states/menu.py
+++ states/menu.py
@@ -14,9 +14,12 @@ class Menu:
         self.display.fill((0, 0, 0))
         self.display.blit(self.image, (0, 0))
 
-        text = self.font.render("Hello, World!", True, (255, 255, 255)) 
-        text_rect = text.get_rect(center=(self.display.get_width() // 2, self.display.get_height() // 2))
+        text = self.font.render("Othello AI", True, (255, 255, 255)) 
+        text_rect = text.get_rect(center=(self.display.get_width() // 2, self.display.get_height() // 3))
+        text2 = self.font.render("Press enter to see simulation", True, (255, 255, 255)) 
+        text_rect2 = text2.get_rect(center=(self.display.get_width() // 2, self.display.get_height() // 2))
 
         self.display.blit(text, text_rect)
+        self.display.blit(text2, text_rect2)
 
         pygame.display.update()
